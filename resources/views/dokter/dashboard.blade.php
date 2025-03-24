39a128b8fc127442c633bc4cfeaebe0fb9755259
joshualungido <joshualungido@gmail.com>
2025-03-24 14:18:39 +0700

    Changed view model controller routes

---
+++ resources/views/dokter/dashboard.blade.php
@@ -0,0 +1,76 @@
+@extends('layout')
+
+@section('content')
+
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Dokter</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Dokter</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Responsive Hover Table</h3>
+
+          <div class="card-tools">
+            <div class="input-group input-group-sm" style="width: 150px;">
+              <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+              <div class="input-group-append">
+                <button type="submit" class="btn btn-default">
+                  <i class="fas fa-search"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body table-responsive p-0">
+          <table class="table table-hover text-nowrap">
+            <thead>
+              <tr>
+                <th>NO</th>
+                <th>ID Periksa</th>
+                <th>Pasien</th>
+                <th>Tanggal Periksa</th>
+                <th>Catatan</th>
+                <th>Biaya Periksa</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach( $periksas as $periksa)
+              <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $periksa->id }}</td>
+                <td>{{ $periksa->pasien->nama }}</td>
+                <td>{{ $periksa->tgl_periksa }}</td>
+                <td>{{ $periksa->catatan }}</td>
+                <td>{{ $periksa->biaya_periksa }}</td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+    </div>
+  </div>
+  <!-- /.row -->
+  <!-- /.content -->
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Models\Periksa;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/dokter', function () {
+    $periksas = Periksa::all();
+    return view('dokter.dashboard', compact('periksas'));
+});
+
+Route::get('/pasien', function () {
+    return view('pasien.dashboard');
+});
+
